76dea18ffcfe3a3890c283748f36d51b0bb56e5e
arash <arashmidos2006@gmail.com>
2018-01-28 11:07:18 +0330

    add doctor

---
+++ app/src/main/java/app/arash/androidcore/data/dao/DoctorDao.java
@@ -0,0 +1,10 @@
+package app.arash.androidcore.data.dao;
+
+import app.arash.androidcore.data.entity.Doctor;
+
+/**
+ * Created by Arash 2018-01-27
+ */
+public interface DoctorDao extends BaseDao<Doctor, Long> {
+
+}
---
+++ app/src/main/java/app/arash/androidcore/data/impl/DoctorDaoImpl.java
@@ -0,0 +1,99 @@
+package app.arash.androidcore.data.impl;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import app.arash.androidcore.data.dao.DoctorDao;
+import app.arash.androidcore.data.entity.Doctor;
+import app.arash.androidcore.data.entity.Drug;
+import app.arash.androidcore.data.entity.MedicDatabaseHelper;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Arash on 1/16/2018.
+ */
+public class DoctorDaoImpl extends AbstractDao<Doctor, Long> implements DoctorDao {
+
+  private Context context;
+
+  public DoctorDaoImpl(Context context) {
+    this.context = context;
+  }
+
+  @Override
+  protected Context getContext() {
+    return context;
+  }
+
+  @Override
+  protected ContentValues getContentValues(Doctor entity) {
+
+    ContentValues contentValues = new ContentValues();
+    contentValues.put(Doctor.COL_ID, entity.getId());
+    contentValues.put(Doctor.COL_NAME, entity.getName());
+    contentValues.put(Doctor.COL_EXPERTISE, entity.getExpertise());
+    contentValues.put(Doctor.COL_PHONE, entity.getPhone());
+    contentValues.put(Doctor.COL_ADDRESS, entity.getAddress());
+
+    return contentValues;
+  }
+
+  @Override
+  protected String getTableName() {
+    return Doctor.TABLE_NAME;
+  }
+
+  @Override
+  protected String getPrimaryKeyColumnName() {
+    return Doctor.COL_ID;
+  }
+
+  @Override
+  protected String[] getProjection() {
+    return new String[]{
+        Doctor.COL_ID,
+        Doctor.COL_NAME,
+        Doctor.COL_EXPERTISE,
+        Doctor.COL_PHONE,
+        Doctor.COL_ADDRESS
+    };
+  }
+
+  @Override
+  protected Doctor createEntityFromCursor(Cursor cursor) {
+    Doctor doctor = new Doctor();
+    doctor.setId(cursor.getLong(0));
+    doctor.setName(cursor.getString(1));
+    doctor.setExpertise(cursor.getString(2));
+    doctor.setPhone(cursor.getString(3));
+    doctor.setAddress(cursor.getString(4));
+
+    return doctor;
+  }
+
+  public List<String> getAllCategory() {
+    MedicDatabaseHelper databaseHelper = MedicDatabaseHelper.getInstance(getContext());
+    SQLiteDatabase db = databaseHelper.getReadableDatabase();
+
+    String[] projections = {Drug.COL_CATEGORY_FA};
+    Cursor cursor = db
+        .query(getTableName(), projections, null, null, Drug.COL_CATEGORY_FA, null, null);
+    List<String> list = new ArrayList<>();
+
+    while (cursor.moveToNext()) {
+      list.add(cursor.getString(0));
+    }
+    cursor.close();
+
+    return list;
+  }
+
+  public List<Doctor> searchByName(String constraint) {
+    String[] args = {constraint};
+    String selection = Doctor.COL_NAME + " =? ";
+    return retrieveAll(selection, args, null, null, null);
+  }
+
+}
